f55bbd132c0b8b3611bb9e5f63e78e98f7729fe8
Rohan Pethiyagoda <sarith21@gmail.com>
2020-02-08 13:25:50 +1100

    Update continent images to incl. dark mode

---
+++ MapQuiz/views/navigationVCs/chooseMapView/ChooseMapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ChooseMapViewController.swift
+//  MapQuiz
+//
+//  Created by Rohan Pethiyagoda on 8/2/20.
+//  Copyright © 2020 Phosphorous Labs. All rights reserved.
+//
+
+import UIKit
+
+class ChooseMapViewController: UITableViewController {
+    
+}
---
+++ MapQuiz/views/navigationVCs/chooseMapView/MapCell.swift
@@ -0,0 +1,9 @@
+//
+//  MapCell.swift
+//  MapQuiz
+//
+//  Created by Rohan Pethiyagoda on 8/2/20.
+//  Copyright © 2020 Phosphorous Labs. All rights reserved.
+//
+
+import Foundation
