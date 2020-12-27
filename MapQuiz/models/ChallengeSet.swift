6cb988c4cd42addc885b2c641f45cb1c5974bb5f
Rohan Pethiyagoda <sarith21@gmail.com>
2020-12-27 08:42:32 +0000

    Add london boroughs

--- MapQuiz/models/ChallengeSet.swift
+++ MapQuiz/models/ChallengeSet.swift
@@ -59,23 +59,24 @@ class ChallengeSetCollection {
 }
 
 class ChallengeSets {
-    //                           slug     title            picker                   tableCellImage              collectionDescriptor                     region                 timeLimit
-    static let NA = ChallengeSet("NA",    "North America", img("pickNorthAmerica"), img("northAmerica"),        "countries & islands",                   region(50, -101, 100), 24*6,  ["NA"])
-    static let SA = ChallengeSet("SA",    "South America", img("pickSouthAmerica"), img("southAmerica"),        "countries",                             region(-19, -60, 100), 13*6,  ["SA"])
-    static let AS = ChallengeSet("AS",    "Asia",          img("pickAsia"),         img("asia"),                "countries",                             region(35, 85, 100),   47*6,  ["AS"])
-    static let EU = ChallengeSet("EU",    "Europe",        img("pickEurope"),       img("europe"),              "countries and city states",             region(60, 10, 100),   46*6,  ["EU"])
-    static let OC = ChallengeSet("OC",    "Oceania",       img("pickOceania"),      img("oceania"),             "countries",                             region(-14, 160, 100), 17*6,  ["OC"])
-    static let AF = ChallengeSet("AF",    "Africa",        img("pickAfrica"),       img("africa"),              "countries",                             region(10, 22, 100),   54*6,  ["AF"])
-    static let WORLD = ChallengeSet("WORLD", "World",      img("pickWorld"),        img("worldIcon"),           "countries, islands and city states",    region(0, 0, 100),     200*6, ["NA", "SA", "AS", "EU", "OC", "AF"])
-    static let US_STATES = ChallengeSet("US_STATES", "US States & Territories",  img("pickUsStates"), img("usStates"), "states & territories",           region(39, -106, 100), 50*6,  ["US_STATES"])
-    static let EN_COUNTIES = ChallengeSet("EN_COUNTIES", "English Counties (Ceremonial)",  img("pickUsStates"), img("usStates"), "ceremonial counties",  region(53, -2, 10),    48*6,  ["englandCounties"])
-    static let SC_COUNTIES = ChallengeSet("SC_COUNTIES", "Scottish Counties (Ceremonial)",  img("pickUsStates"), img("usStates"), "ceremonial counties", region(56, -4, 10),    35*6,  ["scotlandCounties"])
-    static let WA_COUNTIES = ChallengeSet("WA_COUNTIES", "Welsh Counties (Ceremonial)",  img("pickUsStates"), img("usStates"), "ceremonial counties",    region(52, -3.5, 5),   8*6,   ["walesCounties"])
-    static let GBR_COUNTIES = ChallengeSet("GBR_COUNTIES", "All British Counties (Ceremonial)",  img("pickUsStates"), img("usStates"), "ceremonial counties", region(55, -3, 12), 91*6,  ["englandCounties", "scotlandCounties", "walesCounties"])
+    //                                     slug            title                                picker                   tableCellImage       collectionDescriptor                  region             timeLimit  dataFiles
+    static let NA =           ChallengeSet("NA",           "North America",                     img("pickNorthAmerica"), img("northAmerica"), "countries & islands",                region(50, -101, 100), 24*6,  ["NA"])
+    static let SA =           ChallengeSet("SA",           "South America",                     img("pickSouthAmerica"), img("southAmerica"), "countries",                          region(-19, -60, 100), 13*6,  ["SA"])
+    static let AS =           ChallengeSet("AS",           "Asia",                              img("pickAsia"),         img("asia"),         "countries",                          region(35, 85, 100),   47*6,  ["AS"])
+    static let EU =           ChallengeSet("EU",           "Europe",                            img("pickEurope"),       img("europe"),       "countries and city states",          region(60, 10, 100),   46*6,  ["EU"])
+    static let OC =           ChallengeSet("OC",           "Oceania",                           img("pickOceania"),      img("oceania"),      "countries",                          region(-14, 160, 100), 17*6,  ["OC"])
+    static let AF =           ChallengeSet("AF",           "Africa",                            img("pickAfrica"),       img("africa"),       "countries",                          region(10, 22, 100),   54*6,  ["AF"])
+    static let WORLD =        ChallengeSet("WORLD",        "World",                             img("pickWorld"),        img("worldIcon"),    "countries, islands and city states", region(0, 0, 100),     200*6, ["NA", "SA", "AS", "EU", "OC", "AF"])
+    static let US_STATES =    ChallengeSet("US_STATES",    "US States & Territories",           img("pickUsStates"),     img("usStates"),     "states & territories",               region(39, -106, 100), 50*6,  ["US_STATES"])
+    static let EN_COUNTIES =  ChallengeSet("EN_COUNTIES",  "English Counties (Ceremonial)",     img("pickUsStates"),     img("england"),     "ceremonial counties",                region(53, -2, 10),    48*6,  ["englandCounties"])
+    static let SC_COUNTIES =  ChallengeSet("SC_COUNTIES",  "Scottish Counties (Ceremonial)",    img("pickUsStates"),     img("england"),     "ceremonial counties",                region(56, -4, 10),    35*6,  ["scotlandCounties"])
+    static let WA_COUNTIES =  ChallengeSet("WA_COUNTIES",  "Welsh Counties (Ceremonial)",       img("pickUsStates"),     img("england"),     "ceremonial counties",                region(52, -3.5, 5),   8*6,   ["walesCounties"])
+    static let GBR_COUNTIES = ChallengeSet("GBR_COUNTIES", "All British Counties (Ceremonial)", img("pickUsStates"),     img("england"),     "ceremonial counties",                region(55, -3, 12),    91*6,  ["englandCounties", "scotlandCounties", "walesCounties"])
+    static let LONDON =       ChallengeSet("LONDON",       "Boroughs of London",                img("pickUsStates"),     img("england"),     "boroughs",                region(51.5, -0.12, 1),           30*6,  ["london"])
 
     static let all = [
         ChallengeSetCollection(header: "Countries of the World", challengeSets: [NA, SA, AS, EU, OC, AF, WORLD]),
-        ChallengeSetCollection(header: "United Kingdom", challengeSets: [EN_COUNTIES, SC_COUNTIES, WA_COUNTIES, GBR_COUNTIES]),
+        ChallengeSetCollection(header: "United Kingdom", challengeSets: [LONDON, EN_COUNTIES, SC_COUNTIES, WA_COUNTIES, GBR_COUNTIES]),
         ChallengeSetCollection(header: "United States", challengeSets: [US_STATES]),
     ]
 
